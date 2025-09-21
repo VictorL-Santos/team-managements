6830f9768f86629c2966182c44ef3ab1440543e6
Victor_L_Santos <victor.l.o.santos2004@hotmail.com>
2025-09-20 22:58:25 -0300

    fix: fixed product class import

--- src/br/com/anhembi/model/product_owner/ProductOwner.java
+++ src/br/com/anhembi/model/product_owner/ProductOwner.java
@@ -1,8 +1,10 @@
 package br.com.anhembi.model.product_owner;
 
+import br.com.anhembi.model.product.Product;
+
 import java.util.List;
 
 public class ProductOwner {
-    private List<Products> ownedProducts;
+    private List<Product> ownedProducts;
     private String businessUnit;
 }
